3787c132481966eae6d4fe40f34fb34845b47d07
Lord Stoker <tpc00@iesemilidarder.com>
2025-05-12 15:35:56 +0200

    Modificación de gitignore y quitamos contador de Comentarios en h2

--- johny-motorbike/src/components/RouteComments.jsx
+++ johny-motorbike/src/components/RouteComments.jsx
@@ -334,7 +334,7 @@ const RouteComments = ({ ruta, onCommentAdded }) => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
         </svg>
-        Comentarios ({ruta?.comments_count || 0})
+        Comentarios
       </h2>
       
       {/* Formulario para añadir comentario */}
